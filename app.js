68b17649ce799499f977732ce9a7b0eab266e29a
monteiro74 <monteiro74@protonmail.com>
2021-09-03 15:57:37 +0100

    solved lab

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ require('dotenv').config();
 const express = require('express');
 const hbs = require('hbs');
 
+
 // require spotify-web-api-node package here:
+const SpotifyWebApi = require('spotify-web-api-node');
+
 
 const app = express();
 
@@ -12,7 +15,47 @@ app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
 // setting the spotify-api goes here:
-
+const spotifyApi = new SpotifyWebApi({
+    clientId: process.env.CLIENT_ID, // -> possible because of dotenv package installed
+    clientSecret: process.env.CLIENT_SECRET
+  });
+  
+  // Retrieve an access token
+  spotifyApi
+    .clientCredentialsGrant()
+    .then(data => spotifyApi.setAccessToken(data.body['access_token']))
+    .catch(error => console.log('Something went wrong when retrieving an access token', error));
+  
 // Our routes go here:
+
+app.get('/', (req, res) => {
+
+    res.render('index')
+})
+
+app.get('/artist-search', (req, res) => {
+    
+    spotifyApi
+  .searchArtists(req.query.artistName)
+  .then(data => {
+    console.log('The received data from the API: ', data.body.artists.items[0].uri);
+    res.render('artist-search-results', {data});
+  })
+  .catch(err => console.log('The error while searching artists occurred: ', err));
+
+})
+
+app.get('/albums/:id', async (req, res, next) => {
+    const artistsAlbums = await spotifyApi.getArtistAlbums(req.params.id, {limit: 10});
+    console.log(artistsAlbums.body.items[0]);
+    res.render('albums', {artistsAlbums})
+    
+})
+
+app.get('/tracks/:id', async (req, res) => {
+    const albumTracks = await spotifyApi.getAlbumTracks(req.params.id);
+    console.log(albumTracks);
+    res.render('tracks', {albumTracks})
+})
 
 app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
